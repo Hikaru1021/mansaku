e14bce5b79f22cd5aeff8b02e99c222ca86637a1
Hikaru1021 <kagayaki.kirari.1021@gmail.com>
2020-07-02 22:15:40 +0900

    providerパターンでモデルを作ってみる

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:mansaku/next_page.dart';
+import 'package:mansaku/main_model.dart';
+import 'package:provider/provider.dart';
 
-
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  String text = '次へ';
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Pen Time'),
-      ),
-      body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            RaisedButton(
-              child: Text(text),
-              onPressed: () async{
-                // ここに押したら反応するコードをかく
-                //画面遷移のコードをかく
-                final result = await Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => NextPage('Mansaku'),
-                 ),
-                );
-                text = result;
-                print(result);
-              },
-            ),
-          ],
+      home: ChangeNotifierProvider<MainModel>(
+        create: (_) => MainModel(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Pen Time'),
+          ),
+          body: Consumer<MainModel>(builder: (context, model, child) {
+              return Center(
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      model.HikaruText,
+                      style: TextStyle(
+                        fontSize:30,
+                      ),
+                    ),
+                    RaisedButton(
+                      child: Text('ボタン'),
+                      onPressed: (){
+                        model.changeHikaruText();
+                      },
+                    ),
+                  ],
+                ),
+              );
+            }
+          ),
         ),
-      );
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ); // This trailing comma makes auto-formatting nicer for build methods.
-    ;
+      )
+    );
   }
 }
